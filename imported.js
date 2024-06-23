8a0fc5250eae090eeb9caa2f1f2f9f67d324083b
Iwani Izzati Binti Mohd Azdy <iwaniizzati294@gmail.com>
2024-06-23 14:44:31 +0800

    new

--- imported.js
+++ imported.js
@@ -1,12 +1,7 @@
 import navbar from "./componet/navbar.js";
 let nav = document.getElementById("navbar");
-nav.innerHTML=navbar();
-import offers from "./componet/offer.js";
-let offer = document.getElementById("offer");
-offer.innerHTML=offers();
-import search from "./componet/search.js";
-let searchs = document.getElementById("search");
-searchs.innerHTML=search();
+nav.innerHTML = navbar();
+
 import footer from "./componet/footer.js";
 let sfooter = document.getElementById("footer");
-sfooter.innerHTML=footer();
+sfooter.innerHTML = footer();
